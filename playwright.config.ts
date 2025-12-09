243b36b49399e301be32a7dd3ac0f4a4908e360b
Dimitrijevs <dimitrijevgleb@gmail.com>
2025-12-09 18:05:49 +0200

    first commit

---
+++ playwright.config.ts
@@ -0,0 +1,11 @@
+import { defineConfig } from '@playwright/test';
+import { defineBddConfig } from 'playwright-bdd';
+
+const testDir = defineBddConfig({
+  features: 'feature/*.feature',
+  steps: 'steps/**/*.ts',
+});
+
+export default defineConfig({
+  testDir,
+});
