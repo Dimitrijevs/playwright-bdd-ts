ce1eefc023d728cad2536517945b233a1293c321
Dimitrijevs <dimitrijevgleb@gmail.com>
2026-02-27 20:07:45 +0200

    first github action

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,6 +1,9 @@
 import { defineConfig } from '@playwright/test';
 import { defineBddConfig } from 'playwright-bdd';
 
+import dotenv from 'dotenv'; 
+dotenv.config();  // Load environment variables from .env file 
+
 const testDir = defineBddConfig({
   features: 'feature/**/*.feature',
 
@@ -9,6 +12,16 @@ const testDir = defineBddConfig({
 
 export default defineConfig({
   testDir,
+
+  retries: process.env.RETRIES ? parseInt(process.env.RETRIES, 10) : 0,
+  workers: process.env.WORKERS ? parseInt(process.env.WORKERS, 10) : 1,
+
+  use: {
+
+    screenshot: 'only-on-failure',
+    video: 'retain-on-failure',
+    trace: 'retain-on-failure',
+  },
   
   reporter: [
     ['list'],
--- steps/jobApplicationForm/skillsInput.ts
+++ steps/jobApplicationForm/skillsInput.ts
@@ -3,39 +3,61 @@ import { createBdd } from "playwright-bdd";
 
 const { When, Then } = createBdd();
 
-When('User adds the following skills: {string}', async ({page}, skillString: string) => {
-  
+When(
+  "User adds the following skills: {string}",
+  async ({ page }, skillString: string) => {
     // skills
     const skills = skillString.split(",").map((skill) => skill.trim());
 
-    const input = page.locator('div.MuiInputBase-root.MuiOutlinedInput-root.MuiInputBase-colorPrimary.MuiInputBase-fullWidth.MuiInputBase-formControl.css-1syh60j input');
+    const input = page.locator(
+      "div.MuiInputBase-root.MuiOutlinedInput-root.MuiInputBase-colorPrimary.MuiInputBase-fullWidth.MuiInputBase-formControl.css-1syh60j input",
+    );
     for (const skill of skills) {
+
+      await page.waitForTimeout(200);
       await input.pressSequentially(skill, { delay: 200 });
+      
+      await page.waitForTimeout(200);
       await input.press("Enter");
     }
-});
+  },
+);
 
-Then('The skills list shows: {string}', async ({page}, expectedSkills: string) => {
+Then(
+  "The skills list shows: {string}",
+  async ({ page }, expectedSkills: string) => {
+    const expectedSkillsArray = expectedSkills
+      .split(",")
+      .map((skill) => skill.trim());
 
-    const expectedSkillsArray = expectedSkills.split(",").map((skill) => skill.trim());
-  
-    const skillsListLocator = page.locator('div.MuiBox-root.css-e5z02e .MuiChip-label');
+    const skillsListLocator = page.locator(
+      "div.MuiBox-root.css-e5z02e .MuiChip-label",
+    );
 
     const skillInnerTexts = await skillsListLocator.allInnerTexts();
 
     expect(skillInnerTexts).toEqual(expectedSkillsArray);
-});
+  },
+);
 
-When('User removes the skill {string}', async ({page}, removedSkill: string) => {
-  
-    const removedSkillLocator = page.locator('div.MuiBox-root.css-e5z02e > div').filter({ hasText: removedSkill });
+When(
+  "User removes the skill {string}",
+  async ({ page }, removedSkill: string) => {
+    const removedSkillLocator = page
+      .locator("div.MuiBox-root.css-e5z02e > div")
+      .filter({ hasText: removedSkill });
 
-    await removedSkillLocator.locator('svg.MuiChip-deleteIcon').click();
-});
+    await removedSkillLocator.locator("svg.MuiChip-deleteIcon").click();
+  },
+);
 
-Then('The skill {string} is no longer displayed', async ({page}, removedSkill: string) => {
-
-    const removedSkillLocator = page.locator('div.MuiBox-root.css-e5z02e').filter({ hasText: removedSkill });
+Then(
+  "The skill {string} is no longer displayed",
+  async ({ page }, removedSkill: string) => {
+    const removedSkillLocator = page
+      .locator("div.MuiBox-root.css-e5z02e")
+      .filter({ hasText: removedSkill });
 
     await expect(removedSkillLocator).not.toBeVisible();
-});
+  },
+);
